4cb7190ab1055c18854b561a3d1e5b9f8c6aee8d
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-07-28 13:41:07 +0300

    Create cron_service.pp

---
+++ site/profile/manifests/cron_service.pp
@@ -0,0 +1,18 @@
+class profile::cron_service{
+  include cron
+  file { '/home/apostolis/Desktop/my_script/script.sh':
+    path => '/home/apostolis/Desktop/my_script/script.sh'
+    ensure => 'file',
+  }
+  cron::job { 'sys_log':
+    minute      => '*/1',
+    hour        => '*',
+    date        => '*',
+    month       => '*',
+    weekday     => '*',
+    user        => 'root',
+    command     => '/home/apostolis/Desktop/my_script/script.sh -s /var/log/mylog.log',
+    environment => [ 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"', ],
+    description => 'My cron service.',
+  } 
+}
