74c87a639b7feae3a557c68f881a5b891ad7758b
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-07-30 15:59:56 +0300

    Update install_splunk.pp

--- site/profile/manifests/install_splunk.pp
+++ site/profile/manifests/install_splunk.pp
@@ -3,6 +3,6 @@ class profile::install_splunk{
   
   class { 'splunk::enterprise':
   seed_password    => true,
-  password_hash    => '$6$QektizKpHlUUtCcn$dKbY7/582M9GwFVNDpR.kwXFEAo3nuPanUv0H1fTU4kyLksKRS4qASzic6K5Qn20KrsNc3LryLxZEgasgrM1N/ ',
+  password_hash    => '$6$QektizKpHlUUtCcn$dKbY7/582M9GwFVNDpR.kwXFEAo3nuPanUv0H1fTU4kyLksKRS4qASzic6K5Qn20KrsNc3LryLxZEgasgrM1N/',
 }
 }
