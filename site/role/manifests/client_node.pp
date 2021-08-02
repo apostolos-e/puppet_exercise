a018d74dc613a8887657b6d6e4f81a1472987270
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-02 14:05:16 +0300

    Update client_node.pp

--- site/role/manifests/client_node.pp
+++ site/role/manifests/client_node.pp
@@ -1,3 +1,13 @@
 class role::client_node{
+
+  class { '::splunk::params':
+    server => $my_splunk_server,
+    version  => '6.6.8',
+    build    => '6c27a8439c1e',
+    #version  => '7.1.2',
+    #build    => 'a0c72a66db66',
+    src_root => 'https://download.splunk.com',
+  }
+  
   include profile::splunk_forwarder
 }
