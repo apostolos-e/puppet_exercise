3d630b0603e1b798ad5518c541b0c1aeba89d736
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-02 13:09:02 +0300

    Update splunk_forwarder.pp

--- site/profile/manifests/splunk_forwarder.pp
+++ site/profile/manifests/splunk_forwarder.pp
@@ -6,10 +6,4 @@ class profile::splunk_forwarder{
 
   include ::splunk::forwarder
   
-  #@splunkforwarder_input { 'puppetserver-sourcetype':
-  #section => 'monitor:///var/log/puppetlabs/puppetserver/puppetserver.log',
-  #setting => 'sourcetype',
-  #value   => 'puppetserver',
-  #tag     => 'splunk_forwarder'
-  #}
 }
