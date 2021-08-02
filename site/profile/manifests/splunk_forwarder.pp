ca365d73e86a3790a84f9e5ce8e6184f80cf3869
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-02 12:04:23 +0300

    Create splunk_forwarder.pp

---
+++ site/profile/manifests/splunk_forwarder.pp
@@ -0,0 +1,15 @@
+class profile::splunk_forwarder{
+  $my_splunk_server = 'ubuntu-focal'
+  class { '::splunk::params':
+      server => $my_splunk_server,
+  }
+
+  include ::splunk::forwarder
+  
+  @splunkforwarder_input { 'puppetserver-sourcetype':
+  section => 'monitor:///var/log/puppetlabs/puppetserver/puppetserver.log',
+  setting => 'sourcetype',
+  value   => 'puppetserver',
+  tag     => 'splunk_forwarder'
+  }
+}
