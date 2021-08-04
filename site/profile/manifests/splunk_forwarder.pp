82f0f2adc7485fb0df8b7179597c00449866b6b6
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-04 16:07:25 +0300

    Update splunk_forwarder.pp

--- site/profile/manifests/splunk_forwarder.pp
+++ site/profile/manifests/splunk_forwarder.pp
@@ -14,7 +14,7 @@ class profile::splunk_forwarder{
    password_hash    => '$6$QektizKpHlUUtCcn$dKbY7/582M9GwFVNDpR.kwXFEAo3nuPanUv0H1fTU4kyLksKRS4qASzic6K5Qn20KrsNc3LryLxZEgasgrM1N/',
   }  
   
-  splunk::forwarder::inputs.each | $input|{
+  lookup(splunk::forwarder::inputs).each | $input|{
     @splunkforwarder_input { $input['name']:
     section => $input['section'],
     setting => $input['setting'],
