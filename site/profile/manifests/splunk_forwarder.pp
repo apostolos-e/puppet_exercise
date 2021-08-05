cb0937f2209a6712b463c893c9b6abd073ff9fb7
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-05 11:32:32 +0300

    Update splunk_forwarder.pp

--- site/profile/manifests/splunk_forwarder.pp
+++ site/profile/manifests/splunk_forwarder.pp
@@ -15,7 +15,7 @@ class profile::splunk_forwarder{
   }  
   
   $inputs = lookup(splunk::forwarder::inputs)
- $inputs['name'].each $inputs['section'].each | $name, $section| {
+ $inputs['name','section'].each | $name, $section| {
   @splunkforwarder_input { $name:
       section => $section,
       setting => 'sourcetype',
