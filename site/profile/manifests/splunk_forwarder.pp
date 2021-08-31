31b3d6d83e540b01db2a21cf89576c5ae47c32c9
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-08-31 15:21:05 +0300

    Update splunk_forwarder.pp

--- site/profile/manifests/splunk_forwarder.pp
+++ site/profile/manifests/splunk_forwarder.pp
@@ -4,8 +4,8 @@ class profile::splunk_forwarder{
     server => $my_splunk_server,
     #version  => '6.6.8',
     #build    => '6c27a8439c1e',
-    #version  => '7.1.2',
-    #build    => 'a0c72a66db66',
+    version  => '7.1.2',
+    build    => 'a0c72a66db66',
     #src_root => 'https://download.splunk.com',
   }
   
