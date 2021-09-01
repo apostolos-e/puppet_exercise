7adc874d3a18f83b6bbc188590788ec124255830
apostolos-e <87116044+apostolos-e@users.noreply.github.com>
2021-09-01 10:44:49 +0300

    Update splunk_forwarder.pp

--- site/profile/manifests/splunk_forwarder.pp
+++ site/profile/manifests/splunk_forwarder.pp
@@ -2,13 +2,13 @@ class profile::splunk_forwarder{
  $my_splunk_server = 'ubuntu-focal'
   class { '::splunk::params':
     server => $my_splunk_server,
-    #version  => '6.6.8',
-    #build    => '6c27a8439c1e',
+    version  => '6.6.8',
+    build    => '6c27a8439c1e',
     #version  => '7.1.2',
     #build    => 'a0c72a66db66',
-    version  => '8.1.5',
-    build    => '9c0c082e4596',
-    #src_root => 'https://download.splunk.com',
+    #version  => '8.1.5',
+    #build    => '9c0c082e4596',
+    src_root => 'https://download.splunk.com',
   }
   
   class {'splunk::forwarder':
